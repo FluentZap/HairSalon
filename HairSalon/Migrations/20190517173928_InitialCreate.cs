4081c3f6ecc5a78c4185b83dccda0cb67f8fe333
Todd Aden <FluentZap@Gmail.com>
2019-05-17 10:41:57 -0700

    revert to EF 2.2 add Migrations

---
+++ HairSalon/Migrations/20190517173928_InitialCreate.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KrillinStyles.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    Phone_number = table.Column<string>(nullable: true),
+                    Alt_phone_number = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Specialties",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Specialties", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Stylists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    ClientId = table.Column<int>(nullable: true),
+                    Session_id = table.Column<string>(nullable: true),
+                    Login_name = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stylists", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Stylists_Clients_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Clients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StylistSpecialties",
+                columns: table => new
+                {
+                    StylistId = table.Column<int>(nullable: false),
+                    SpecialtyId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StylistSpecialties", x => new { x.StylistId, x.SpecialtyId });
+                    table.ForeignKey(
+                        name: "FK_StylistSpecialties_Specialties_SpecialtyId",
+                        column: x => x.SpecialtyId,
+                        principalTable: "Specialties",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StylistSpecialties_Stylists_StylistId",
+                        column: x => x.StylistId,
+                        principalTable: "Stylists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stylists_ClientId",
+                table: "Stylists",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StylistSpecialties_SpecialtyId",
+                table: "StylistSpecialties",
+                column: "SpecialtyId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StylistSpecialties");
+
+            migrationBuilder.DropTable(
+                name: "Specialties");
+
+            migrationBuilder.DropTable(
+                name: "Stylists");
+
+            migrationBuilder.DropTable(
+                name: "Clients");
+        }
+    }
+}
