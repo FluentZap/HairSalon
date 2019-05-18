9e11351c6982e4b60fa387facd4898b9ab857b9a
Todd Aden <FluentZap@Gmail.com>
2019-05-17 18:32:06 -0700

    add redirect to edit page from client list by trylist

--- HairSalon/Controllers/ClientController.cs
+++ HairSalon/Controllers/ClientController.cs
@@ -123,7 +123,7 @@ namespace KrillinStyles.Controllers
 
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
-		{
+		{			
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 		}
 	}
